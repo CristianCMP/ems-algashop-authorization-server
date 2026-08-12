c023c2c68971b35f1babba67d6ea5417757d45fb
Cristian Puhl <cristian.puhl@universo.univates.br>
2026-08-12 11:44:13 -0300

    - Token for password change and email verification

--- src/main/java/com/algaworks/algashop/authorizationserver/domain/model/user/AuthUser.java
+++ src/main/java/com/algaworks/algashop/authorizationserver/domain/model/user/AuthUser.java
@@ -7,6 +7,8 @@ import jakarta.persistence.*;
 import lombok.*;
 import org.apache.commons.lang3.StringUtils;
 
+import java.time.Duration;
+import java.time.OffsetDateTime;
 import java.util.Objects;
 import java.util.UUID;
 
@@ -27,6 +29,11 @@ public class AuthUser extends AbstractAuditableAggregateRoot<AuthUser> {
     private String password;
     private String name;
     private boolean enabled;
+    private boolean emailVerified;
+
+    private String verificationToken;
+
+    private OffsetDateTime verificationTokenExpirationDate;
 
     @Enumerated(EnumType.STRING)
     private AuthUserType type;
@@ -34,30 +41,35 @@ public class AuthUser extends AbstractAuditableAggregateRoot<AuthUser> {
     public static AuthUser brandNew(String email,
                                     String name,
                                     AuthUserType type,
-                                    String passwordHash) {
+                                    AuthUserPasswordManager passwordManager) {
         AuthUser user = new AuthUser();
 
         user.setId(IdGenerator.generateTimeBasedUUID());
         user.setEmail(email);
         user.setName(name);
         user.setType(type);
-        user.setPassword(passwordHash);
+        user.setPassword(passwordManager.encrypt(passwordManager.generates()));
         user.setEnabled(true);
+        user.setEmailVerified(false);
 
         return user;
+    }
+
+    public String generateVerificationToken(Duration expiresIn, VerificationTokenHasher hasher) {
+        String plainToken = hasher.generate();
+        this.verificationToken = hasher.hash(plainToken);
+        this.verificationTokenExpirationDate = OffsetDateTime.now().plus(expiresIn);
+        return plainToken;
+    }
+
+    public boolean isDisabled() {
+        return !isEmailVerified() || !isEnabled();
     }
 
     public void anonymize() {
         this.setName("Anonymized User");
         this.setEmail("anonymized-" + this.id + "@deleted.local");
         this.setEnabled(false);
-    }
-
-    public void setPassword(String password) {
-        if (StringUtils.isBlank(password)) {
-            throw new IllegalArgumentException();
-        }
-        this.password = password;
     }
 
     public void setName(String name) {
@@ -79,6 +91,13 @@ public class AuthUser extends AbstractAuditableAggregateRoot<AuthUser> {
         this.type = type;
     }
 
+    private void setPassword(String password) {
+        if (StringUtils.isBlank(password)) {
+            throw new IllegalArgumentException();
+        }
+        this.password = password;
+    }
+
     private void setId(UUID id) {
         Objects.requireNonNull(id);
         this.id = id;
@@ -89,5 +108,9 @@ public class AuthUser extends AbstractAuditableAggregateRoot<AuthUser> {
             throw new IllegalArgumentException();
         }
         this.email = email;
+    }
+
+    private void setEmailVerified(boolean emailVerified) {
+        this.emailVerified = emailVerified;
     }
 }

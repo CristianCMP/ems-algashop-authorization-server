e478e6ee863de6db4d86238f7c02a861cc2505a8
Cristian Puhl <cristian.puhl@universo.univates.br>
2026-08-12 00:10:02 -0300

    - Configuring default redirection after login

--- src/main/java/com/algaworks/algashop/authorizationserver/infrastructure/security/AuthorizationServerSecurityConfig.java
+++ src/main/java/com/algaworks/algashop/authorizationserver/infrastructure/security/AuthorizationServerSecurityConfig.java
@@ -104,7 +104,9 @@ public class AuthorizationServerSecurityConfig {
         http.authorizeHttpRequests(authorize -> authorize
                         .requestMatchers("/login","/forgot-password","/css/**","/js/**","/img/**","/favicon.ico").permitAll()
                         .anyRequest().authenticated())
-                .formLogin(c -> c.loginPage("/login").permitAll());
+                .formLogin(c -> c.loginPage("/login")
+                        .defaultSuccessUrl(properties.getDefaultRedirectUri())
+                        .permitAll());
         return http.build();
     }
 }
